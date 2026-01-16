c5ca366d66cced575b90a0be59a6e5125bdb748e
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-01-17 01:59:00 +0300

    Inital commit: Started GrayTherapy project

---
+++ src/main/java/RiotGetAPI.java
@@ -0,0 +1,12 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class RiotGetAPI {
+    public static void main(String[] args) {
+
+    }
+}
