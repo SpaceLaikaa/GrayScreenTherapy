71aac398a2f5e7e60cee105445aa2920e9223e1d
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-01-17 18:22:52 +0300

    implement guard clauses for null exception and method-based architecture for stable death detection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,36 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Main {
+    static boolean isYouTubeOpen = false;
+    static double totalDeadTime = 0;
+
+    public static void aliveLogic(double currentHealth){
+        if (isYouTubeOpen) {
+            System.out.println("✅ ALIVE -> Welcome back!");
+            isYouTubeOpen = false;
+        }
+        System.out.println("ALIVE (Health: " + currentHealth + ")");
+    }
+
+    public static void deathLogic(double respawnTimer){
+        totalDeadTime++;
+        if (!isYouTubeOpen && respawnTimer>10) {
+            System.out.println("Starting Therapy Session...");
+            String genericShortsUrl = "https://www.youtube.com/shorts";
+
+            try {
+                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "start", genericShortsUrl);// To go directly to yt shorts
+                pb.start();
+                isYouTubeOpen = true;
+                Thread.sleep(1000);
+
+            } catch (Exception e) {
+                System.err.println("Error opening browser: " + e.getMessage());
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        boolean isYouTubeOpen = false;
         System.out.println("---GrayScreenTherapy Starting");
         System.out.println("Trying to find The LoL Client...");
 
@@ -19,7 +46,7 @@ public class Main {
         while(true){
             try{
                 HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create("https://127.0.0.1:2999/liveclientdata/allgamedata"))
+                        .uri(URI.create("https://127.0.0.1:2999/liveclientdata/allgamedata"))//Gets riot api
                         .GET()
                         .build();
 
@@ -27,36 +54,34 @@ public class Main {
                 String responseBody = response.body();
 
                 JsonObject root = JsonParser.parseString(responseBody).getAsJsonObject();
+
+                if(!root.has("activePlayer") || root.get("activePlayer").isJsonNull()){
+                    System.out.println("⚠️Waiting for data (ActivePlayer)...");
+                    Thread.sleep(1000);
+                    continue;
+                }
                 JsonObject activePlayer = root.getAsJsonObject("activePlayer");
+
+                if (!activePlayer.has("championStats") || !activePlayer.has("respawnTimer")) {
+                    System.out.println("⚠️Waiting for data (Stats)...");
+                    Thread.sleep(1000);
+                    continue;
+                }
                 JsonObject championStats = activePlayer.getAsJsonObject("championStats");
+
                 double currentHealth = championStats.get("currentHealth").getAsDouble();
+                double respawnTimer = activePlayer.get("respawnTimer").getAsDouble();
 
                 if (currentHealth <= 0) {
-                    if (!isYouTubeOpen) {
-                        System.out.println("Starting Therapy Session...");
-                        String genericShortsUrl = "https://www.youtube.com/shorts";
-
-                        try {
-                            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "start", genericShortsUrl);// To go directly to yt shorts
-                            pb.start();
-                            isYouTubeOpen = true;
-                            Thread.sleep(1000);
-
-                        } catch (Exception e) {
-                            System.err.println("Error opening browser: " + e.getMessage());
-                        }
-                    }
+                    deathLogic(respawnTimer);
                 } else {
-                    if (isYouTubeOpen) {
-                        System.out.println("✅ ALIVE -> Welcome back!");
-                        isYouTubeOpen = false;
-                    }
-                    System.out.println("ALIVE (Health: " + currentHealth + ")");
+                    aliveLogic(currentHealth);
                 }
 
                 Thread.sleep(1000);
             } catch (Exception e){
                 System.out.println("❌ Could not connect to LoL. Is the game running? ❌");
+                e.printStackTrace();
                 try {
                     Thread.sleep(2000);
                 } catch (InterruptedException ex) {
